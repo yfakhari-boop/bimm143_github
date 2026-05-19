9d9b404893926926792f0339fd2a05a5f4ad430b
Yassaman Fakhari <yfakhari@ucsd.edu>
2026-05-19 12:43:33 -0700

    add class 4-7

---
+++ class04/class04.R
@@ -0,0 +1,10 @@
+#' ---
+#' title: "Class4: Intro to R"
+#' author: "Yassaman Fakhari (PID:18541145)"
+#' date: "April 9, 2026"
+#' ---
+#this is just a text file 
+x <- 1:50
+plot(x,sin(x),type="l",col="blue",lwd=3)
+
+
